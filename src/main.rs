4cee5fe6dcc2f3243e12e158b9f645021a254c38
kmolski <krzysztof.molski29@gmail.com>
2023-06-28 08:50:38 +0200

    Add Tokio & Axum

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::SocketAddr;
+
+use axum::{Router, Server};
+use axum::response::Html;
+use axum::routing::get;
+
+#[tokio::main]
+async fn main() {
+    let router = Router::new().route("/", get(|| async { Html("Hello World!") }));
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    Server::bind(&addr)
+        .serve(router.into_make_service())
+        .await;
 }
