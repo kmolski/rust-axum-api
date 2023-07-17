01b8057471046d4c5f23b0497f6803178db01dc3
kmolski <krzysztof.molski29@gmail.com>
2023-07-17 20:59:13 +0200

    Add file encryption with openssl

--- src/crypto.rs
+++ src/crypto.rs
@@ -1,1 +1,66 @@
+use std::sync::Arc;
 
+use openssl::{rsa, sha, symm};
+use openssl::rsa::Padding;
+
+const SALT: [u8; 16] = [
+    102, 0, 21, 56, 172, 34, 123, 32, 45, 250, 6, 42, 80, 66, 190, 200,
+];
+
+#[derive(Clone)]
+pub struct KeyManager {
+    aes_key: Arc<String>,
+    rsa_pub: Arc<String>,
+    rsa_priv: Arc<String>,
+}
+
+impl KeyManager {
+    pub fn new(aes_key: String, rsa_pub: String, rsa_priv: String) -> Self {
+        KeyManager {
+            aes_key: Arc::new(aes_key),
+            rsa_pub: Arc::new(rsa_pub),
+            rsa_priv: Arc::new(rsa_priv),
+        }
+    }
+
+    pub fn encrypt_aes(&self, data: &[u8]) -> Vec<u8> {
+        let cipher = symm::Cipher::aes_256_ctr();
+        let key = sha::sha256(self.aes_key.as_bytes());
+
+        symm::encrypt(cipher, &key, Some(&SALT), data).unwrap()
+    }
+
+    pub fn decrypt_aes(&self, data: &[u8]) -> Vec<u8> {
+        let cipher = symm::Cipher::aes_256_ctr();
+        let key = sha::sha256(self.aes_key.as_bytes());
+
+        symm::decrypt(cipher, &key, Some(&SALT), data).unwrap()
+    }
+
+    pub fn encrypt_rsa(&self, data: &[u8]) -> Vec<u8> {
+        let key = rsa::Rsa::public_key_from_pem_pkcs1(self.rsa_pub.as_bytes()).unwrap();
+        let mut encrypted = Vec::new();
+        for chunk in data.chunks(128) {
+            let mut buf = [0; 256];
+            key.public_encrypt(chunk, &mut buf, Padding::PKCS1_OAEP)
+                .unwrap();
+            encrypted.extend_from_slice(&buf);
+        }
+
+        encrypted
+    }
+
+    pub fn decrypt_rsa(&self, data: &[u8]) -> Vec<u8> {
+        let key = rsa::Rsa::private_key_from_pem(self.rsa_priv.as_bytes()).unwrap();
+        let mut decrypted = Vec::new();
+        for chunk in data.chunks(256) {
+            let mut buf = [0; 256];
+            let read = key
+                .private_decrypt(chunk, &mut buf, Padding::PKCS1_OAEP)
+                .unwrap();
+            decrypted.extend_from_slice(&buf[..read]);
+        }
+
+        decrypted
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,33 @@
 use std::env;
+use std::fs::File;
+use std::io::{Error, Read};
 use std::net::SocketAddr;
+use std::path::Path;
 
-use axum::extract::DefaultBodyLimit;
-use axum::routing::get;
 use axum::{Router, Server};
+use axum::extract::{DefaultBodyLimit, FromRef};
+use axum::routing::get;
 
 use crate::controller::*;
-use crate::file_manager::*;
+use crate::crypto::KeyManager;
+use crate::file_manager::FileManager;
 
 mod controller;
 mod crypto;
 mod file_manager;
+
+#[derive(Clone, FromRef)]
+pub struct AppState {
+    file_manager: FileManager,
+    key_manager: KeyManager,
+}
+
+fn read_key_file(path: impl AsRef<Path>) -> Result<String, Error> {
+    let mut buf = String::new();
+    let mut file = File::open(path)?;
+    file.read_to_string(&mut buf)?;
+    Ok(buf)
+}
 
 #[tokio::main]
 async fn main() {
@@ -21,11 +38,25 @@ async fn main() {
     let address = SocketAddr::from(([127, 0, 0, 1], 3000));
     eprintln!("Example app listening on port {}", address.port());
 
+    let aes_key_val = env::var("CRYPT_AES_KEY").expect("Error: No AES key given");
+    let rsa_pub_path = env::var("RSA_PUB_PEM");
+    let rsa_priv_path = env::var("RSA_PRIV_PEM");
+    let rsa_pub = read_key_file(rsa_pub_path.as_deref().unwrap_or("./public.pem"))
+        .expect("Could not read public key");
+    let rsa_priv = read_key_file(rsa_priv_path.as_deref().unwrap_or("./private.pem"))
+        .expect("Could not read private key");
+
     let file_manager = FileManager::new(base_dir);
+    let key_manager = KeyManager::new(aes_key_val, rsa_pub, rsa_priv);
+    let app_state = AppState {
+        file_manager,
+        key_manager,
+    };
+
     let router = Router::new()
-        .merge(routes_streaming_encryption(file_manager.clone()))
-        .merge(routes_sync_encryption(file_manager.clone()))
-        .merge(routes_no_encryption(file_manager.clone()))
+        .merge(routes_streaming_encryption(app_state.clone()))
+        .merge(routes_sync_encryption(app_state.clone()))
+        .merge(routes_no_encryption(app_state.clone()))
         .route("/", get(|| async { "Hello World!" }))
         .layer(DefaultBodyLimit::max(1024_usize.pow(3))); // 1 gigabyte
     Server::bind(&address)
